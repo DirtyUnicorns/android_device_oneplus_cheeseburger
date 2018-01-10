1a619bd09423f2c1cccd02a34a771448f1aaaf4c
Josh Chasky <jchasky@gmail.com>
2018-01-09 19:06:18 -0500

    Add overlays for hw keys and button light support
    Change-Id: Ie9b8a4b0baf7ce6bd88c9529c515f9f9c401a0fe

--- device.mk
+++ device.mk
@@ -24,6 +24,8 @@
 
 $(call inherit-product, vendor/oneplus/cheeseburger/cheeseburger-vendor.mk)
 
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := xxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
